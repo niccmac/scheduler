404be7400cf62eed131979ca5210ad8ce6c8c874
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-19 17:38:19 -0700

    pass setDay state down to daylistitem

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import DayListItem from "./DayListItem";
 
 const days = [
   {
@@ -21,6 +22,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -33,8 +36,9 @@ export default function Application(props) {
         <nav className="sidebar__menu">
         <DayList
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay= {setDay}
+          
         />
         </nav>
         <img
