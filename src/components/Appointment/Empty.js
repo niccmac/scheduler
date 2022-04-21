ab6ab4170cddc3aed598221299f679e7423917ec
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-20 18:35:19 -0700

    Make changes to add button

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-export default function(props) {
+export default function (props) {
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-button"
-        src="image/add.png"
-        alt="add"
-        onClick={ props.onAdd }
+        src="images/add.png"
+        alt="Add"
       />
     </main>
   )
