e8d6ca9af506dbd8a7f3ce6ef2aca48d86a77f50
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-20 19:22:18 -0700

    Form Edit Button events

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,15 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    props.onCancel()
+  }
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,7 +37,10 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={ props.onCancel } danger>Cancel</Button>
+          <Button onClick={() => {
+            reset(); 
+            cancel()
+          }} danger>Cancel</Button>
           <Button onClick={ props.onSave }confirm>Save</Button>
         </section>
       </section>
