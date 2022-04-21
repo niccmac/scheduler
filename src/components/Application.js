2894dfd3057a18b5e0ae3526473d557bffb22a3f
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-21 15:39:59 -0700

    Add axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios  from "axios";
 
 const appointments = {
   "1": {
