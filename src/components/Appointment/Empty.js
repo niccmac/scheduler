5d864fe22b7f0df2565c1ca75010bc6446e3ad46
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-20 17:49:16 -0700

    Add header, empty and show to appointments plus add SB tests

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function(props) {
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="image/add.png"
+        alt="add"
+        onClick={props.onAdd}
+      />
+    </main>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -5,11 +5,15 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
+import Appointment from "components/Appointment/index.js";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList"
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList"
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 storiesOf("Button", module)
   .addParameters({
@@ -136,3 +140,18 @@ storiesOf("Button", module)
           onChange={action("setInterviewer")}
         />
       ));
+
+      storiesOf("Appointment", module)
+      .addParameters({
+        backgrounds: [{name: "white", value: "#fff", default: true}]
+      })
+      .add("Appointment", () => <Appointment />)
+      .add("Appointment with time", () => <Appointment time="12pm"/>)
+      .add("Header", () => <Header time="12pm" />)
+      .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+      .add("Show", () => <Show 
+        student="Lydia Miller-Jones"
+        interviewer={interviewers[0].name}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+       />)
