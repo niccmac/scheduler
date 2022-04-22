cb2ef0e944064accf2793eddd2b79844cdce0c42
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-21 17:23:30 -0700

    Days and Appointments state update together in azios promise

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,46 +4,6 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios  from "axios";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-
 export default function Application(props) {
   
   const [ state, setState ] = useState({
@@ -51,18 +11,25 @@ export default function Application(props) {
     days: [], 
     appointments: {}
   })
-
-  const daysURL = `http://localhost:8001/api/days`;
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
-  useEffect(() => axios.get(daysURL)
-  .then((response) => setDays(response.data))
-  .catch((error) => {
-    console.log(`Axios Day URL get error status:`,error.response.status);
-    console.log(`Axios Day URL get error header:`,error.response.header);
-    console.log(`Axios Day URL get error data:`,error.response.data);
-  }),[]);
-
+  
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  
+  const dailyAppointments = [];
+  const URLS = {
+    "GET_DAYS":     `http://localhost:8001/api/days`,
+    "GET_APPOINTMENTS": `http://localhost:8001/api/appointments`,
+    "GET_INTERVIEWERS": `http://localhost:8001/api/interviewers`
+  }
+  useEffect(() => {
+    Promise.all([
+      axios.get(URLS.GET_DAYS),
+      axios.get(URLS.GET_APPOINTMENTS)
+      // axios.get(URLS.GET_INTERVIEWERS)
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+    })
+  }, [])
+   
   return (
     <main className="layout">
       <section className="sidebar">
@@ -76,7 +43,7 @@ export default function Application(props) {
           <DayList
             days={state.days}
             value={state.day}
-            onChange={"in here"}
+            onChange={setDay}
           />
         </nav>
         <img
@@ -86,7 +53,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        { Object.values(appointments).map((values) => <Appointment
+        { dailyAppointments.map((values) => <Appointment
           key={values}
           id={values.id}
           time={values.time}
