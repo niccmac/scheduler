a9a53a5929a7836d3adc9409c186d46ca806f068
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-22 17:41:49 -0700

    Add useVisual hook to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "components/Appointment/styles.scss";
 import Show from "./Show";
 import Empty from "./Empty";
@@ -6,13 +6,19 @@ import Header from "./Header";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-const { interview } = props;
+  const { interview } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
 
   return (
   <article className="appointment">
     <Header time={ props.time } />
-    { props.interview && <Show student={ interview.student } interviewer={ interview.interviewer.name } />}
-    { !props.interview && <Empty />}
+    { props.interview && <Show student={ interview.student } interviewer={ interview.interviewer.name } /> }
+    { !props.interview && <Empty /> }
   </article>
   )
 };
